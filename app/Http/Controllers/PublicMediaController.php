f55d71abf8410bf077448f60b17ccb0d8f376832
Nirjon Roy <roynirjon18@gmail.com>
2026-03-10 02:26:10 +0600

    User images no longer need Apache to serve

--- app/Http/Controllers/PublicMediaController.php
+++ app/Http/Controllers/PublicMediaController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class PublicMediaController extends Controller
 {
-    public function show(string $path): Response
+    public function show(string $path): BinaryFileResponse
     {
         $path = trim(str_replace('\\', '/', $path), '/');
 
